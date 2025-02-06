d776ea4ef096d2781a461b3208e97d958d67f3c2
LukasGockingLorente2004 <lukasgockinglorente@gmail.com>
2025-02-06 10:35:51 +0100

    Metodes tabla

--- src/Table.java
+++ src/Table.java
@@ -1,6 +1,6 @@
 public class Table {
 
-    private char[][] table = new char[6][7];
+    private char [][] table = new char[6][7];
 
     public Table() {
         this.table = new char[6][7]; // Crea un array vacío (inicializado con '\0')
@@ -9,5 +9,41 @@ public class Table {
                 table[i][j] = ' '; // Llena el tablero con espacios en blanco
             }
         }
+    }
+
+    public char[][] getTable() {
+        return table;
+    }
+
+    public void setTable(char[][] table) {
+        this.table = table;
+    }
+
+    public static void printTable (char [][] table){
+        for (int i = 0; i < table.length; i++) {
+            for (int j = 0; j < table[0].length; j++) {
+                System.out.print( "|" + table[i][j] + "|");
+            }
+            System.out.println();
+            System.out.println("---------------------");
+        }
+        System.out.println();
+    }
+
+    public static char [][] placeToken (char [][] taula, int pos1, int pos2, char token){
+        if (taula[pos1][pos2] == ' '){
+            taula[pos1][pos2] = token;
+        }
+
+        return taula;
+    }
+
+    public static char [][] resetTable (char [][] table){
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 7; j++) {
+                table[i][j] = ' '; // Llena el tablero con espacios en blanco
+            }
+        }
+        return table;
     }
 }
