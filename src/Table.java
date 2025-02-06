3f6c4eb0089a68e9efbe1d3d1118ba3213e3cf8c
LukasGockingLorente2004 <lukasgockinglorente@gmail.com>
2025-02-06 10:33:58 +0100

    Inicialitzacio tabla

--- src/Table.java
+++ src/Table.java
@@ -1,5 +1,13 @@
 public class Table {
 
+    private char[][] table = new char[6][7];
 
-
+    public Table() {
+        this.table = new char[6][7]; // Crea un array vacío (inicializado con '\0')
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 7; j++) {
+                table[i][j] = ' '; // Llena el tablero con espacios en blanco
+            }
+        }
+    }
 }
